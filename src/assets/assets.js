67c0f03d55138b58f085debe5e7bae6b9dcf62b5
VivekNakkana <vivekkumarnakkana@gmail.com>
2023-03-24 11:12:04 +0530

    Initial Commit

---
+++ src/assets/assets.js
@@ -0,0 +1,6 @@
+import cartImg from "./cart.png";
+import paymentLogo from "./paymentLogo.png";
+import googleLogo from "./googleLogo.png";
+import githubLogo from "./githubLogo.png";
+
+export { cartImg, paymentLogo, googleLogo, githubLogo };
---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1cbnq8ICUmg7_QQyE0ZVAorrcMW2ouOQ",
+  authDomain: "gocci-17634.firebaseapp.com",
+  projectId: "gocci-17634",
+  storageBucket: "gocci-17634.appspot.com",
+  messagingSenderId: "845735182733",
+  appId: "1:845735182733:web:b30ff6db98338ea430bdc4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
